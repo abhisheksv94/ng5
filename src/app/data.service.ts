c1183220f9c21c3b456bb75a29641fb0fac9793a
Abhishek Shrinivasan <abhishek@Abhisheks-MacBook-Pro.local>
2018-05-20 17:40:15 -0400

    first commit

---
+++ src/app/data.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable()
+export class DataService {
+  private goals= new BehaviorSubject<any>(['Initial goal','Another silly life goal']);
+  goal=this.goals.asObservable();
+
+  constructor() { }
+
+  changeGoal(goal){
+    this.goals.next(goal);
+  }
+}
